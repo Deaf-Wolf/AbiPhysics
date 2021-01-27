090e41167e3dc4e3b6d2ca0c1b7b1328b9aebdaa
Deaf-Wolf <58134413+Deaf-Wolf@users.noreply.github.com>
2021-01-27 19:35:46 +0100

    3 Neue Formeln

---
+++ Franck - Hertz - Versuch.py
@@ -0,0 +1,23 @@
+#Konstanten#
+c = (2.997925E+8)
+h = (6.6262E-34)
+e = (1.6022E-19)
+#Konstanten#
+
+print("ohne Leertasten und auf Großschreibung achten")
+print("Wellenlänge oder Strahlung")
+Auswahl = input()
+
+if Auswahl == "Wellenlänge":
+    #Berrechnet die Wellenlänge
+    U=float(input("U="))
+    Wl=f"{(h*c)/e*U}"
+    print("λ=",Wl)
+elif Auswahl == "Strahlung":
+    Wl = float(input("λ="))
+    Eel= f"{h*c/Wl}"
+    print("Eel=",Eel)
+
+
+
+LogOut = input("Exit: Klick Enter")  # Beenden des Programmes
---
+++ λ mit Rydberg-Konstanten.py
@@ -0,0 +1,15 @@
+#Konstanten#
+c = 2.997925E+8
+fR= 3.28984195E+15
+#Konstanten#
+n=float(input("n")) #n eingeben _ Normal immer 2
+
+m=float(input("m")) #m eingeben _ immer grösser 2
+
+Wellenlänge=float(f"{c/fR*(1/(1/n**2 - 1/m**2))}")
+
+
+
+
+print("λ=",abs(piko))
+
